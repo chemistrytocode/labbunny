3eb326eadb0776c59e61ec0ae84623c4136afcd5
Paul <paul.ayling@bglgroup.co.uk>
2019-03-07 20:30:12 +0000

    Added TableSorter, data starting to display in table

---
+++ src/components/TableData/TableSorter.js
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+
+// import upcomingWeekInterface from '../../interface/upcomingWeekInterface'
+
+import "./tableData.css";
+
+class TableSorter extends Component {
+  render() {
+    return (
+      <div>
+        {this.props.p1practicals.map(eachPractical => {
+          return (
+            <div id="eachPost">
+              <div id id="innerPost">
+                <p id="userFeed">{eachPractical.teacher}</p>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
+}
+
+export default TableSorter;
--- src/components/Upcoming/upcomingWeek.js
+++ src/components/Upcoming/upcomingWeek.js
@@ -1,11 +1,135 @@
 import React, { Component } from "react";
+import TableSorter from '../TableData/TableSorter'
 import NavBar from '../NavBar/navbar'
+
 // import upcomingWeekInterface from '../../interface/upcomingWeekInterface'
 
 import "./upcomingStyling.css";
 
+
 class UpcomingWeek extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      practicals: []
+    };
+  }
+
+
+  componentDidMount() {
+    fetch("https://localhost:5001/api/Acebook")
+      .then(response => response.json())
+      .then(practicals => {
+        this.setState({ practicals: practicals });
+      });
+  }
+
+
   render() {
+    // const practicals = this.state
+    // Mocked Practicals
+    const practicals = [
+      {
+        teacher: "Paul",
+        period: "1",
+        dateReq: "2019-03-04"
+      },
+      {
+        teacher: "James",
+        period: "2",
+        dateReq: "2019-03-05"
+      },
+      {
+        teacher: "Tabitha",
+        period: "3",
+        dateReq: "2019-03-06"
+      },
+      {
+        teacher: "Chris",
+        period: "4",
+        dateReq: "2019-03-07"
+      },
+      {
+        teacher: "Megan",
+        period: "5",
+        dateReq: "2019-03-08"
+      },
+      {
+        teacher: "Andy",
+        period: "6",
+        dateReq: "2019-03-09"
+      },
+      {
+        teacher: "Robin",
+        period: "7",
+        dateReq: "2019-03-11"
+      },
+      {
+        teacher: "Jonathan",
+        period: "8",
+        dateReq: "2019-03-12"
+      }
+    ];
+    const p1m = []
+    const p1tu = []
+    const p1w = []
+    const p1th = []
+    const p1f = []
+    const p1s = []
+    const p2m = []
+    const p2tu = []
+    const p2w = []
+    const p2th = []
+    const p2f = []
+    const p2s = []
+    const p3m = []
+    const p3tu = []
+    const p3w = []
+    const p3th = []
+    const p3f = []
+    const p3s = []
+    const p4m = []
+    const p4tu = []
+    const p4w = []
+    const p4th = []
+    const p4f = []
+    const p4s = []
+    const p5m = []
+    const p5tu = []
+    const p5w = []
+    const p5th = []
+    const p5f = []
+    const p5s = []
+    const p6m = []
+    const p6tu = []
+    const p6w = []
+    const p6th = []
+    const p6f = []
+    const p6s = []
+    const p7m = []
+    const p7tu = []
+    const p7w = []
+    const p7th = []
+    const p7f = []
+    const p7s = []
+    const p8m = []
+    const p8tu = []
+    const p8 = []
+    const p8th = []
+    const p8f = []
+    const p8s = []
+    practicals.map(eachPractical => {
+      if (eachPractical.period === "1") { p1m.push(eachPractical) }
+      if (eachPractical.period === "2") { p2m.push(eachPractical) }
+      if (eachPractical.period === "3") { p3m.push(eachPractical) }
+      if (eachPractical.period === "4") { p4m.push(eachPractical) }
+      if (eachPractical.period === "5") { p5m.push(eachPractical) }
+      if (eachPractical.period === "6") { p6m.push(eachPractical) }
+      if (eachPractical.period === "7") { p7m.push(eachPractical) }
+      if (eachPractical.period === "8") { p8m.push(eachPractical) }
+    })
+
     return (
       <div id="upcomingContainer" class="col-sm-12">
         <h2> Upcoming Orders! </h2>
@@ -49,7 +173,7 @@ class UpcomingWeek extends Component {
               </tr>
               <tr class="table-light">
                 <td id="LRborder">1</td>
-                <td id="p1m" />
+                <td id="p1m"><TableSorter p1practicals ={p1m}/></td>
                 <td id="p1tu" />
                 <td id="p1w" />
                 <td id="p1th" />
@@ -58,7 +182,7 @@ class UpcomingWeek extends Component {
               </tr>
               <tr class="table-light">
                 <td id="LRborder">2</td>
-                <td id="p2m" />
+                <td id="p2m"><TableSorter p1practicals ={p2m}/></td>
                 <td id="p2tu" />
                 <td id="p2w" />
                 <td id="p2th" />
@@ -67,7 +191,7 @@ class UpcomingWeek extends Component {
               </tr>
               <tr class="table-light">
                 <td id="LRborder">3</td>
-                <td id="p3m" />
+                <td id="p3m"><TableSorter p1practicals ={p3m}/></td>
                 <td id="p3tu" />
                 <td id="p3w" />
                 <td id="p3th" />
@@ -81,7 +205,7 @@ class UpcomingWeek extends Component {
               </tr>
               <tr class="table-light">
                 <td id="LRborder">4</td>
-                <td id="p4m" />
+                <td id="p4m"><TableSorter p1practicals ={p4m}/></td>
                 <td id="p4tu" />
                 <td id="p4w" />
                 <td id="p4th" />
@@ -90,7 +214,7 @@ class UpcomingWeek extends Component {
               </tr>
               <tr class="table-light">
                 <td id="LRborder">5</td>
-                <td id="p5m" />
+                <td id="p5m"><TableSorter p1practicals ={p5m}/></td>
                 <td id="p5tu" />
                 <td id="p5w" />
                 <td id="p5th" />
@@ -104,7 +228,7 @@ class UpcomingWeek extends Component {
               </tr>
               <tr class="table-light">
                 <td id="LRborder">6</td>
-                <td id="p6m" />
+                <td id="p6m"><TableSorter p1practicals ={p6m}/></td>
                 <td id="p6tu" />
                 <td id="p6w" />
                 <td id="p6th" />
@@ -113,7 +237,7 @@ class UpcomingWeek extends Component {
               </tr>
               <tr class="table-light">
                 <td id="LRborder">7</td>
-                <td id="p7m" />
+                <td id="p7m"><TableSorter p1practicals ={p7m}/></td>
                 <td id="p7tu" />
                 <td id="p7w" />
                 <td id="p7th" />
@@ -122,7 +246,7 @@ class UpcomingWeek extends Component {
               </tr>
               <tr class="table-light">
                 <td id="LRborder">8</td>
-                <td id="p8m" />
+                <td id="p8m"><TableSorter p1practicals ={p8m}/></td>
                 <td id="p8tu" />
                 <td id="p8w" />
                 <td id="p8th" />
